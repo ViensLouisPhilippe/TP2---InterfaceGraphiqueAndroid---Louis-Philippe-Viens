7ae7f03fb83e25b92ae6a4ff8df3f3135604b5b1
Louis-Philippe Viens <2034421@cegepmontpetit.ca>
2023-10-19 10:40:49 -0400

    PROGRES Écran des résultat et page d'accueil
    il reste l'ecran des resultat à completer sinon la page d'accueil est complet

---
+++ TP1/app/src/main/java/com/example/tp1/ActivityResultat.java
@@ -0,0 +1,96 @@
+package com.example.tp1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ActivityResultat extends AppCompatActivity {
+    BarChart chart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultat);
+        setTitle("Résultats");
+
+
+        chart = findViewById(R.id.chart);
+        /* Settings for the graph - Change me if you want*/
+        chart.setMaxVisibleValueCount(6);
+        chart.setPinchZoom(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setGranularity(1);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setValueFormatter(new DefaultAxisValueFormatter(0));
+        chart.getDescription().setEnabled(false);
+        chart.getAxisRight().setEnabled(false);
+
+
+
+        /* Data and function call to bind the data to the graph */
+        Map<Integer, Integer> dataGraph = new HashMap<Integer, Integer>() {{
+            put(3, 2);
+            put(4, 1);
+            put(5, 4);
+            put(1, 0);
+            put(2, 0);
+            put(0, 0);
+        }};
+        setData(dataGraph);
+    }
+
+
+    private void setData(Map<Integer, Integer> datas) {
+        List<BarEntry> values = new ArrayList<>();
+        /* Every bar entry is a bar in the graphic */
+        for (Map.Entry<Integer, Integer> key : datas.entrySet()){
+            values.add(new BarEntry(key.getKey() , key.getValue()));
+        }
+
+        BarDataSet set1;
+        if (chart.getData() != null &&
+                chart.getData().getDataSetCount() > 0) {
+            set1 = (BarDataSet) chart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            chart.getData().notifyDataChanged();
+            chart.notifyDataSetChanged();
+        } else {
+            set1 = new BarDataSet(values, "Points");
+
+            set1.setDrawIcons(false);
+
+            ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+            dataSets.add(set1);
+
+            BarData data = new BarData(dataSets);
+            data.setValueTextSize(10f);
+            data.setBarWidth(.9f);
+            chart.setData(data);
+        }
+    }
+}
